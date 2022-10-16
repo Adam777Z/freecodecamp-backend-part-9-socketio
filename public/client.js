4162613fc6d4964d4da8d190a2701c09c647d4ff
Adam777 <Adam777-5867@outlook.com>
2022-10-16 17:05:30 +0000

    Update

--- public/client.js
+++ public/client.js
@@ -1,27 +1,31 @@
-$(document).ready(function() {
-  /*global io*/
-  var socket = io();
-  
-  socket.on('user', function(data) {
-    $('#num-users').text(data.currentUsers + ' users online');
-    var message = data.name;
-    if (data.connected) {
-      message += ' has joined the chat.';
-    } else {
-      message += ' has left the chat.';
-    }
-    $('#messages').append($('<li>').html('<b>' + message + '<\/b>'));
-  });
-  
-  socket.on('chat message', function(data) {
-    $('#messages').append($('<li>').text(data.name + ': ' + data.message));
-  });
-  
-  // Form submission with new message in field with id 'm'
-  $('form').submit(function() {
-    var messageToSend = $('#m').val();
-    socket.emit('chat message', messageToSend); //send message to server here
-    $('#m').val('');
-    return false; // prevent form submit from refreshing page
-  });
+document.addEventListener('DOMContentLoaded', (event) => {
+	/* global io */
+	var socket = io();
+
+	socket.on('user', (data) => {
+		document.querySelector('#num-users').textContent = data['currentUsers'] + ' users online';
+
+		let message = data['name'];
+
+		if (data['connected']) {
+			message += ' has joined the chat.';
+		} else {
+			message += ' has left the chat.';
+		}
+
+		document.querySelector('#messages').innerHTML += '<li><b>' + message + '</b></li>';
+	});
+
+	socket.on('chat message', (data) => {
+		document.querySelector('#messages').innerHTML += '<li>' + data['name'] + ': ' + data['message'] + '</li>';
+	});
+
+	// Form submission with new message in field with id 'm'
+	document.querySelector('form').addEventListener('submit', (event2) => {
+		event2.preventDefault(); // prevent form submit from refreshing page
+
+		let messageToSend = document.querySelector('#m').value;
+		socket.emit('chat message', messageToSend); // send message to server here
+		document.querySelector('#m').value = '';
+	});
 });
